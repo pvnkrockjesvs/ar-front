b6cc50f5487edcad45c223822dd1074439fa72ef
Roger <roger.nguyen@hotmail.fr>
2023-05-15 09:35:43 +0200

    front - Artist page with css

---
+++ components/Artist.js
@@ -0,0 +1,13 @@
+import styles from "../styles/Artist.module.css";
+
+function Artist() {
+  return (
+    <div className={styles.mainContainer}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Artist Page</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Artist;
