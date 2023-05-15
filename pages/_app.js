456fe10dbbe1cac669c85b561bc104c51f30a044
Roger <roger.nguyen@hotmail.fr>
2023-05-15 16:18:44 +0200

    front - Artist Page and css

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Next.js App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
