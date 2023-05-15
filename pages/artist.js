17e2f26fe05bc06e32cddb2dc040ea36124d5782
Roger <roger.nguyen@hotmail.fr>
2023-05-15 17:15:19 +0200

    front - Artist Page and css

--- pages/artist.js
+++ pages/artist.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Artist from "../components/Artist";
+import Head from "next/head";
 
 function ArtistPage() {
-  return <Artist />;
+  return (
+    <>
+      <Head>
+        <title>Artist Page</title>
+      </Head>
+      <Artist />
+    </>
+  );
 }
 
 export default ArtistPage;
