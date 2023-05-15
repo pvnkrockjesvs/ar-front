6c98944aee768721dbbe56910eef701a7a22ffb9
Roger <roger.nguyen@hotmail.fr>
2023-05-15 09:58:31 +0200

    front - Artist Page and css

--- components/Artist.js
+++ components/Artist.js
@@ -1,11 +1,28 @@
 import styles from "../styles/Artist.module.css";
+import Image from "next/image";
 
 function Artist() {
   return (
     <div className={styles.mainContainer}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Artist Page</h1>
-      </main>
+      <div className={styles.leftContainer}>
+        <Image
+          src="/artist.jpg"
+          alt="Artist picture"
+          width={120}
+          height={120}
+        />
+        <p>Last Release : </p>
+        <Image
+          src="/Watch-the-throne.jpg"
+          alt="Album cover"
+          width={120}
+          height={120}
+        />
+        <p>Album Title</p>
+        <p>2023</p>
+      </div>
+      <div className={styles.textContainer}></div>
+      <div className={styles.discographyContainer}></div>
     </div>
   );
 }
---
+++ pages/artist.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Artist from "../components/Artist";
+
+function ArtistPage() {
+  return <Artist />;
+}
+
+export default ArtistPage;
